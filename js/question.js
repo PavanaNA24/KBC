94c664871335bb432c4a93d606c7f0f16377b709
Pavana <pavanabharath1324@gmail.com>
2021-02-05 12:23:13 +0530

    files added

---
+++ js/question.js
@@ -0,0 +1,29 @@
+const quiz = [
+{
+	q:'Which of the following option leads to the portability and security of Java?',
+	options:['Bytecode is executed by JVM','The applet makes the Java code secure and portable','Use of exception handling','Dynamic binding between objects'],
+	answer:0
+},
+{
+	q:'Which of the following is not a Java features?',
+	options:['Dynamic','Architecture Neutral','Use of pointers','Object-oriented'],
+	answer:2
+},
+{
+	q:'_____ is used to find and fix bugs in the Java programs.',
+	options:['JVM','JRE','JDK','JDB'],
+	answer:3
+},
+{
+	q:'Which of the following is a valid long literal?',
+	options:['ABH8097','L990023','904423','0xnf029L'],
+	answer:0
+},
+{
+	q:'What is the return type of the hashCode() method in the Object class?',
+	options:['Object','int','long','void'],
+	answer:1
+}
+
+
+]
